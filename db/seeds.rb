26dd51f789102da7727baeb09d1b7d2ec1ec4020
Sergey Drogan <drawgun@yandex.ru>
2023-03-23 23:55:57 +0900

    update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-require_relative '../models/user'
+require_relative '../config/environment'
 
-User.destroy_all
+Following.dataset.destroy
+SleepPeriod.dataset.destroy
+User.dataset.destroy
 
 %w(Bob Joahn Nik).each do |name|
   User.create(name: name)
